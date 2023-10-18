991ec79c522a213149cfb940667135ad32e9220f
khloud54 <kh.t705042000@gmail.com>
2023-10-18 01:39:45 -0700

    atoi

--- _atoi.c
+++ _atoi.c
@@ -40,18 +40,17 @@ int _isalpha(int c)
 
 /**
  *_atoi - converts a string to an integer
- *@s: the string to be converted
- *Return: 0 if no numbers in string, converted number otherwise
- */
-
+ * @s: the string to be converted
+ * Return: 0 if no numbers in string, converted number otherwise
+*/
 int _atoi(char *s)
 {
 	int i, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 
-	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
 	{
-		if (s[i] == '-')
+		if(s[i] == '-')
 			sign *= -1;
 
 		if (s[i] >= '0' && s[i] <= '9')
