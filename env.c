7b95f6fb85e32732933b079977a0a8cb88c094ab
khloud54 <kh.t705042000@gmail.com>
2023-10-19 08:41:43 -0700

    ev

---
+++ env.c
@@ -0,0 +1,12 @@
+#include "file.h"
+#include <stdio.h>
+
+/**
+ * print_env - Function to print enviroment variables
+ * @vars: Argument
+ * Return: Nothing
+*/
+void print_env(vars_t *vars)
+{
+	(void)vars;
+}
